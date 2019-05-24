48ea31537643249c11b52c53efcb67e5bdd548f3
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-24 16:29:58 +0900

    Initial Commit

---
+++ NetworkChecker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NetworkChecker
+//
+//  Created by yosuke on 2019/05/24.
+//  Copyright © 2019 yosuke.nakayama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
