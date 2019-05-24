9e3530b7b1c18c40aa4219d6aecc86ce7c17660f
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2019-05-24 16:56:29 +0900

    UI Update

--- NetworkChecker/AppDelegate.swift
+++ NetworkChecker/AppDelegate.swift
@@ -13,19 +13,41 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    @IBOutlet weak var IPAddr1: NSTextField!
+    
+    @IBOutlet weak var StartBtn: NSButtonCell!
+    @IBOutlet weak var StopBtn: NSButton!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification)
+    {
         // Insert code here to initialize your application
+        StartBtn.isEnabled = true;
+        StopBtn.isEnabled = false;
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
+    func applicationWillTerminate(_ aNotification: Notification)
+    {
         // Insert code here to tear down your application
     }
     
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool
+    {
+        /* ウインドウを閉じた時にアプリも終了させる */
         return true;
     }
 
 
+    @IBAction func Start(_ sender: Any)
+    {
+        StartBtn.isEnabled = false;
+        StopBtn.isEnabled = true;
+    }
+    
+    
+    @IBAction func Stop(_ sender: Any)
+    {
+        StartBtn.isEnabled = true;
+        StopBtn.isEnabled = false;
+    }
 }
 
